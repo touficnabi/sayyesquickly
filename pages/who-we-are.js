7e124c24ca92988bb3894b7a54d8592362525f48
Toufic Nabi <toufic.nabi@gmail.com>
2021-07-12 09:53:34 -0400

    quotation and apos fix

--- pages/who-we-are.js
+++ pages/who-we-are.js
@@ -25,7 +25,7 @@ const WhoWeAre = () => {
                         <Row className="justify-content-center">
                             <Col md={9}>
                                 <h5 className="text-center">
-                                    Russell Martin and his colleagues bring many years of experience to their work in print, television, and film. Beautiful Faces is a &quot;splendid documentary&quot; according to Reforma, Mexico&apos;s leading newspaper. The documentary film Two Spirits was broadcast during the 2010-2011 season of the PBS series &quot;Independent Lens,&quot; and won the season&apos;s Audience Award. Russell Martin's bestselling book projects--including Beethoven&apos;s Hair, Picasso&apos;s War, and Out of Silence--have been translated into dozens of languages throughout the world.
+                                    Russell Martin and his colleagues bring many years of experience to their work in print, television, and film. Beautiful Faces is a &quot;splendid documentary&quot; according to Reforma, Mexico&apos;s leading newspaper. The documentary film Two Spirits was broadcast during the 2010-2011 season of the PBS series &quot;Independent Lens,&quot; and won the season&apos;s Audience Award. Russell Martin&apos;s bestselling book projects--including Beethoven&apos;s Hair, Picasso&apos;s War, and Out of Silence--have been translated into dozens of languages throughout the world.
                                 </h5>
                             </Col>
                         </Row>
