70d70396aee7c465c4b25fcd163f8878515dc6b4
Toufic Nabi <toufic.nabi@uberflip.com>
2021-12-31 02:33:53 -0500

    scroll issue in header fixed

--- component/Nav.js
+++ component/Nav.js
@@ -23,25 +23,25 @@ export default function Nav() {
     useEffect(() => {
         if (!isMobile){
             gsap.to(navbar.current, {backgroundColor: '#ffffeb', scrollTrigger: {
-                trigger: 'header',
+                trigger: '.very-top',
                 start: 'top top',
                 scrub: true,
                 end: '+=300px'
             }})
             gsap.to(nav1.current, {x: -60, color: '#485543', scrollTrigger: {
-                trigger: 'header',
+                trigger: '.very-top',
                 start: 'top top',
                 scrub: true,
                 end: '+=300px'
             }})
             gsap.to(nav2.current, {x: 53, color: '#485543', scrollTrigger: {
-                trigger: 'header',
+                trigger: '.very-top',
                 start: 'top top',
                 scrub: true,
                 end: '+=300px'
             }})
             gsap.from(logoImg.current, { y: '-170%', scrollTrigger:{
-                trigger: 'header',
+                trigger: '.very-top',
                 start: 'top top',
                 scrub: true,
                 end: '+=300px'
@@ -54,28 +54,31 @@ export default function Nav() {
     }
 
     return(
-        <header className={Styles.header}>
-            <a className="top" id="top"></a>
-            <a className={Styles.mobile_nav} onClick={mobile_nav_open}>
-                {!mobileNavIsOpen ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faTimes} />}
-            </a>
-            <Navbar ref={navbar} className={ !mobileNavIsOpen ? `${Styles.navbar} p-0` : `${Styles.navbar} p-0 ${Styles.mobile_open}`}>
-                <Container className="justify-content-center">
-                    <ul ref={nav1} className={`${Styles.main_nav} ${Styles.nav_1}`}>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/">Home</Link></li>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/who-we-are">Who We Are</Link></li>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/russell-martin">Russell Martin</Link></li>
-                    </ul>
-                    <div ref={logoImg} className={`${Styles.logo} logo`}><Image src={Logo} alt="Say Yes Quickly Logo" styles={{borderBottomRightRadius: '4px', borderBottomLeftRadius: '4px'}} /></div>
-                    {/* <img ref={logoImg} src="img/logo.jpeg" alt="Say Yes Quickly Logo" className={`${Styles.logo} logo`} /> */}
-                    <ul ref={nav2} className={`${Styles.main_nav} ${Styles.nav_2}`}>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/news">News</Link></li>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/books">Books</Link></li>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/films">Films</Link></li>
-                        <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/#">Articles</Link></li>
-                    </ul>
-                </Container>
-            </Navbar>
-        </header>
+        <>
+            <div className="very-top"></div>
+            <header className={Styles.header}>
+                <a className="top" id="top"></a>
+                <a className={Styles.mobile_nav} onClick={mobile_nav_open}>
+                    {!mobileNavIsOpen ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faTimes} />}
+                </a>
+                <Navbar ref={navbar} className={ !mobileNavIsOpen ? `${Styles.navbar} p-0` : `${Styles.navbar} p-0 ${Styles.mobile_open}`}>
+                    <Container className="justify-content-center">
+                        <ul ref={nav1} className={`${Styles.main_nav} ${Styles.nav_1}`}>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/">Home</Link></li>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/who-we-are">Who We Are</Link></li>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/russell-martin">Russell Martin</Link></li>
+                        </ul>
+                        <div ref={logoImg} className={`${Styles.logo} logo`}><Image src={Logo} alt="Say Yes Quickly Logo" styles={{borderBottomRightRadius: '4px', borderBottomLeftRadius: '4px'}} /></div>
+                        {/* <img ref={logoImg} src="img/logo.jpeg" alt="Say Yes Quickly Logo" className={`${Styles.logo} logo`} /> */}
+                        <ul ref={nav2} className={`${Styles.main_nav} ${Styles.nav_2}`}>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/news">News</Link></li>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/books">Books</Link></li>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/films">Films</Link></li>
+                            <li className={Styles.nav_item} onClick={mobile_nav_open}><Link href="/#">Articles</Link></li>
+                        </ul>
+                    </Container>
+                </Navbar>
+            </header>
+        </>
     )
 }
