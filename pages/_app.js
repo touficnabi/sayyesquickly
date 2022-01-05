2c9e785366cc32c4deb9c42cb835e7a0f8c615d9
Toufic Nabi <toufic.nabi@uberflip.com>
2022-01-05 15:12:19 -0500

    preloader added (kinda)

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import gsap from 'gsap';
 import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
 import Layout from '../component/Layout';
+import Router from 'next/router'
+import { useEffect, useRef, useState } from 'react';
 
 gsap.registerPlugin(ScrollTrigger)
 
 function MyApp({ Component, pageProps }) {
-    return( 
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+
+    const [isLoading, setInLoading] = useState(false);
+    const load = useRef(null)
+
+    useEffect(() => {
+        gsap.to(load.current, {
+            alpha: 0, 
+            display: "none",
+            duration: .5
+        })
+        Router.events.on('routeChangeStart', (url) => {
+            setInLoading(true);
+            gsap.to(load.current, {
+                alpha: 1, 
+                display: "block",
+                duration: .5
+            })
+        })
+        Router.events.on('routeChangeComplete', (url) => {
+            setInLoading(false)
+            gsap.to(load.current, {
+                alpha: 0, 
+                display: "none",
+                duration: .5
+            })
+        })
+    }, [])
+
+    return(
+        <>
+            <div className="loading" ref={load}></div>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
 
