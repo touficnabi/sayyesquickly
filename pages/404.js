96bffd27ee96e4e1de24ddc1d32b514c7745ba3a
Toufic Nabi <toufic.nabi@uberflip.com>
2022-06-21 03:27:53 -0400

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import renderHTML from 'react-render-html';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Custom404 = ({pageContent}) => {
+    console.log('tofi', pageContent);
+    return (
+        <>
+            <Head>
+                <title>Sorry! The page is not available - SayYesQuickly</title>
+            </Head>
+            <main style={{marginTop: 79}}>
+                <section>
+                    <Container>
+                        <Row>
+                            <Col lg={12}>
+                                <div className="text-center">
+                                    {renderHTML(pageContent.content.rendered)}    
+                                </div> 
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export const getStaticProps = async () => {
+    const custom404 = await fetch('https://cms.sayyesquickly.net/wp-json/wp/v2/pages/318');
+    const pageContent = await custom404.json();
+
+    return {
+        props: {
+            pageContent
+        },
+        revalidate: 10
+    }
+}
+
+export default Custom404;
